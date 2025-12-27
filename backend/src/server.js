c7e467604582a0bd64fe0e19e1dda870ba0fbe04
alfardan <alfardanarfin500@gmail.com>
2025-12-27 11:20:33 +0530

    Admin routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,8 @@ import { serve } from "inngest/express";
 
 import { functions, inngest } from "./config/inngest.js";
 
+import adminRoutes from "./routes/admin.route.js"
+
 const app = express();
 
 const __dirname = path.resolve();
@@ -15,6 +17,8 @@ app.use(express.json());
 app.use(clerkMiddleware()); // add auth object under the req=>req.auth
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.use("/api/admin",adminRoutes)
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
