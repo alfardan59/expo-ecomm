107f4292b884c58e0eb823a51dcef575c4e890f9
alfardan <alfardanarfin500@gmail.com>
2025-12-30 18:39:59 +0530

    fix in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
-import { clerkMiddleware } from '@clerk/express';
-import { serve } from "inngest/express";
-import cors from "cors";
-
-import { functions, inngest } from "./config/inngest.js";
+import { inngest, functions } from "./config/inngest.js";
 
 import adminRoutes from "./routes/admin.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -16,36 +16,35 @@ import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 
 const app = express();
-
 const __dirname = path.resolve();
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+/* ✅ INNGEST MUST BE FIRST + RAW BODY */
+app.use(
+  "/api/inngest",
+  express.raw({ type: "application/json" }),
+  serve({ client: inngest, functions })
+);
 
+/* ✅ NORMAL MIDDLEWARE AFTER */
 app.use(express.json());
-app.use(clerkMiddleware()); // add auth object under the req=>req.auth
-app.use(cors({origin:ENV.CLIENT_URL, credentials:true}))
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware());
 
-
-app.use("/api/admin",adminRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/orders",orderRoutes);
-app.use("/api/reviews",reviewRoutes);
-app.use("/api/products",productRoutes);
-app.use("/api/cart",cartRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
 });
 
-
-
-//make out app ready for deployment
-
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
-
   app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../admin/dist/index.html"));
   });
 }
 
