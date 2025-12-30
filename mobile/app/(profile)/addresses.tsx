c0c978c98513c034fcedf5d1f6ce4186e08206b2
alfardan <alfardanarfin500@gmail.com>
2025-12-30 14:48:09 +0530

    Wishlist ui updated

---
+++ mobile/app/(profile)/addresses.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import SafeScreen from '@/components/SafeScreen'
+
+const addresses = () => {
+  return (
+    <SafeScreen>
+      <Text className='text-white'>addresses</Text>
+    </SafeScreen>
+  )
+}
+
+export default addresses
---
+++ mobile/app/(profile)/privacy-security.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import SafeScreen from '@/components/SafeScreen'
+
+const PrivacyandSecurity = () => {
+  return (
+    <SafeScreen>
+      <Text className='text-white'>privacy-security</Text>
+    </SafeScreen>
+  )
+}
+
+export default PrivacyandSecurity
