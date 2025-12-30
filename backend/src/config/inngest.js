6ec81a164dc6f88ff2974613af7e5f7eb8603d86
alfardan <alfardanarfin500@gmail.com>
2025-12-30 19:10:00 +0530

    fix

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,26 +1,27 @@
-import {Inngest} from "inngest";
-import {connectDB} from "./db.js";
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
 
-export const inngest=new Inngest({id:"ecomm-app"});
+export const inngest = new Inngest({ id: "ecommerce-app" });
 
-const syncUser=inngest.createFunction(
-    {id:"sync-user"},
-    {event:"clerk/user.created"},
-    async({event})=>{
-        await connectDB();
-        const {id, email_addresses, first_name, last_name, image_url}=event.data;
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
-        const newUser={
-            clerkId: id,
-            email: email_addresses[0]?.email_address,
-            name: `${first_name || ""} ${last_name || ""}` || "User",
-            imageUrl: image_url,
-            addresses: [],
-            wishlist: [],
-          };
-        await User.create(newUser);
-    }
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}` || "User",
+      imageUrl: image_url,
+      addresses: [],
+      wishlist: [],
+    };
+
+    await User.create(newUser);
+  }
 );
 
 const deleteUserFromDB = inngest.createFunction(
@@ -34,4 +35,4 @@ const deleteUserFromDB = inngest.createFunction(
   }
 );
 
-export const functions=[syncUser, deleteUserFromDB]
+export const functions = [syncUser, deleteUserFromDB];
